4250447b8ba15ef77d15439f1e8854a1237d3205
Michael Sliger <michaelj.sliger@gmail.com>
2020-09-18 09:25:28 -0600

    setup?

---
+++ src/bookmarkRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bookmarkRouter = express.Router();
+const logger = require('./logger');
+
+
+bookmarkRouter 
+    .route('/')
+    .get((req, res) => {
+
+    })
+    .post((req, res) => {
+
+    });
---
+++ src/dataStore.js
@@ -0,0 +1,9 @@
+const BOOKMARKS = [
+  {
+      title: 'Google',
+      rating: 5,
+      url: 'https://google.com',
+      desc: 'A serach engine'
+  }
+];
+// title, rating, url, desc
